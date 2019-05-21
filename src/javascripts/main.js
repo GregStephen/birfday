01c42168e957f199de890a97c5f6fa5e3a5b5b16
Greg Stephen <GregAStephen@gmail.com>
2019-05-20 19:54:58 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,13 @@
+import firebase from 'firebase/app';
 import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
+import apiKeys from './helpers/apiKeys.json';
 
 const init = () => {
+  console.error('keys', apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
