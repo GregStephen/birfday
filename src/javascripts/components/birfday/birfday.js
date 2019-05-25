92a943b5b3694f88d9a897a92108c602f2cf68a9
Greg Stephen <GregAStephen@gmail.com>
2019-05-25 11:30:14 -0500

    prints out

--- src/javascripts/components/birfday/birfday.js
+++ src/javascripts/components/birfday/birfday.js
@@ -3,10 +3,11 @@ import birfdayData from '../../helpers/data/birfdayData';
 
 const birfdayStringBuilder = (uid) => {
   birfdayData.getBirfdayByUid(uid)
-    .then((resp) => {
-      console.error(resp);
+    .then((birthday) => {
       let domstring = '';
-      domstring += '<h2>BIRFDAY</h2>';
+      domstring += `<h1>${birthday.date}</h1>`;
+      domstring += `<img src="${birthday.imageUrl}" alt="birthday location">`;
+      domstring += `<h3>${birthday.location} @ ${birthday.time}</h3>`;
       util.printToDom('event', domstring);
     }).catch(err => console.error('could not get birfday', err));
 };
--- src/javascripts/helpers/data/birfdayData.js
+++ src/javascripts/helpers/data/birfdayData.js
@@ -4,11 +4,15 @@ import apiKeys from '../apiKeys.json';
 const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getBirfdayByUid = uid => new Promise((resolve, reject) => {
-  console.error(uid);
   axios.get(`${firebaseUrl}/birthdays.json?orderBy="uid"&equalTo="${uid}"`)
     .then((results) => {
       const birthdayResults = results.data;
-      console.error(birthdayResults);
+      const birthdays = [];
+      Object.keys(birthdayResults).forEach((birthdayId) => {
+        birthdayResults[birthdayId].id = birthdayId;
+        birthdays.push(birthdayResults[birthdayId]);
+      });
+      resolve(birthdays[0]);
     })
     .catch(err => reject(err));
 });
